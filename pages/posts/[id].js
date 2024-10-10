7363fb069350f31eb619605731cc77ab6ce94607
FehSouza <fernanda.souza142010@gmail.com>
2024-10-10 19:08:41 -0300

    feat: page post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,31 +1,30 @@
-import { getGlobalData } from '../../utils/global-data'
-import { getPostBySlug } from '../../utils/mdx-utils'
-import Head from 'next/head'
-import CustomLink from '../../components/CustomLink'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Layout, { GradientBackground } from '../../components/Layout'
 import SEO from '../../components/SEO'
+import { getGlobalData } from '../../utils/global-data'
+import { getPostBySlug } from '../../utils/mdx-utils'
 
-const components = {
-  a: CustomLink,
-  Head,
-}
-
-export default function PostPage({ posts, globalData }) {
+export default function PostPage({ post, globalData }) {
   return (
     <Layout>
-      <SEO title={`${posts.title} - ${globalData.name}`} description={posts.description} />
+      <SEO title={`${post.title ?? 'Not Found'} - ${globalData.name}`} description={post.description ?? 'Not Found'} />
       <Header name={globalData.name} />
-      <article className="px-6 md:px-0">
-        <header>
-          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">{posts?.title}</h1>
-          {posts?.description && <p className="text-xl mb-4">{posts?.description}</p>}
-        </header>
-        <main>
-          <article className="prose dark:prose-dark">{posts.body}</article>
-        </main>
-      </article>
+
+      {!post.id && <span className="prose dark:prose-dark">Infelizmente, esse post não foi encontrado em nossa base de dados...</span>}
+
+      {!!post.id && (
+        <article className="px-6 md:px-0">
+          <header>
+            <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">{post?.title}</h1>
+            {post?.description && <p className="text-xl mb-4">{post?.description}</p>}
+          </header>
+          <main>
+            <article className="prose dark:prose-dark">{post.body}</article>
+          </main>
+        </article>
+      )}
+
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground variant="large" className="absolute -top-32 opacity-30 dark:opacity-50" />
       <GradientBackground variant="small" className="absolute bottom-0 opacity-20 dark:opacity-10" />
@@ -35,12 +34,7 @@ export default function PostPage({ posts, globalData }) {
 
 export const getServerSideProps = async ({ params }) => {
   const globalData = getGlobalData()
-  const posts = await getPostBySlug(params.id)
+  const post = await getPostBySlug(params.id)
 
-  return {
-    props: {
-      globalData,
-      posts,
-    },
-  }
+  return { props: { globalData, post } }
 }
